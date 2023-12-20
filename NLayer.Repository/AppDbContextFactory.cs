408040f5efc7f08ee6cd973a6fdb0a54e32bfa50
xeaydin <53emiraydin53@gmail.com>
2023-12-20 22:38:00 +0300

    Initial commit

---
+++ NLayer.Repository/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NLayer.Repository;
+
+public class AppDbContextFactory:IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer("Server=ISTN37687;Database=UdemyNLayerDb;User=xeaydin;Password=Password12.;TrustServerCertificate=true");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
